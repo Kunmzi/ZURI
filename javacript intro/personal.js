1ef68d7c5f50d3139f35faf8b2d8e2a032f3e05a
Kunmzi <98911363+Kunmzi@users.noreply.github.com>
2022-06-25 21:48:53 +0100

    Add files via upload

---
+++ javacript intro/personal.js
@@ -0,0 +1,7 @@
+let whatareyoucalled = 'Ogunwa Olukunmi';
+let howtallareyou = '5ft 1inch';
+let whatisyournationality = 'Nigeria';
+
+console.log(whatareyoucalled);
+console.log(howtallareyou);
+console.log(whatisyournationality);
